5d3981821267ea11485eab6cbd58aae1c80a478c
jasondebolt <jasondebolt@gmail.com>
2017-05-04 18:22:21 -0700

    adding my chef-client cookbook wrapper

--- metadata.rb
+++ metadata.rb
@@ -4,12 +4,14 @@ maintainer_email 'jasondebolt@gmail.com'
 license 'all_rights'
 description 'Installs/Configures chef-centos7-chef-client'
 long_description 'Installs/Configures chef-centos7-chef-client'
-version '0.1.0'
+version '0.1.1'
 
 issues_url 'https://github.com/jasondebolt/chef-centos7-chef-client/issues' if respond_to?(:issues_url)
 
 source_url 'https://github.com/jasondebolt/chef-centos7-chef-client' if respond_to?(:source_url)
 
 depends 'chef-centos7-common'
-depends 'chef-centos7-workstation'
+#depends 'chef-centos7-workstation'
 depends 'chef-centos7-httpd'
+
+depends 'chef-client', '= 7.0.2'
